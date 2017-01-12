66d2eb0f58ed607ff7bc23615b3c8ce8a14bbc14
Adam <AdamBCo@me.com>
2017-01-12 07:02:04 -0800

    Added compliance/notification features

--- Eula.framework/Headers/ELAManager.h
+++ Eula.framework/Headers/ELAManager.h
@@ -11,6 +11,9 @@
 
 @interface ELAManager : NSObject
 
+
+#pragma mark - Account Information
+
 /**
  Initialize Eula with your SDK key and secret.
  Call this method in application:didFinishLaunchingWithOptions:
@@ -21,18 +24,67 @@
 
 + (void)setAPIKey:(NSString *)apiKey apiSecret:(NSString *)apiSecret;
 
-// Acount Specifc Getters
+/**
+ Your accounts APIKey.
+ **/
 + (NSString *)apiKey;
-+ (NSString *)apiSecret;
-    
-#pragma mark - App Information
 
-+ (void)setAppName:(NSString *)appName;
-+ (NSString *)appName;
+/**
+ Your accounts APISecret.
+ **/
++ (NSString *)apiSecret;
+
+
+#pragma mark - User Information
+
+/**
+ Sets the email for the User.
+ @discussion This method is required if you want to use the Notification Features on `https://eula.io/`.
+**/
++ (void)setUserEmail:(NSString *)email;
+
+/**
+ Sets the name for the User.
+ **/
++ (void)setUserName:(NSString *)name;
+
+/**
+ Sets the imageURL for the User.
+ **/
++ (void)setUserImageUrl:(NSString *)url;
+
+/**
+ The email for the 'User.
+**/
++ (NSString *)userEmail;
+
+/**
+ The name for the `User`.
+**/
++ (NSString *)userName;
+
+/**
+ The imageURL for the `User`.
+**/
++ (NSString *)userImageUrl;
+
+
+/**
+ Enables the compliance/notification features on `https://eula.io/`
+ @discussion A users email must be set to use this feature.
+ */
++(void)activateApp;
 
 #pragma mark - View Controllers
 
+/**
+ A sublcass of UIViewController that shows your Terms of Serrvice.
+**/
 + (UIViewController *)termsOfServiceViewController;
+
+/**
+ A sublcass of UIViewController that shows your Terms of Serrvice.
+ **/
 + (UIViewController *)privacyPolicyViewController;
 
 @end
